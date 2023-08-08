7a5ad9ec74550bb2239172dc225885fc55225869
Jiawei Ye <vaye.ye@gmail.com>
2023-08-08 10:21:58 +0800

    final test

--- app_rd/do_reduction.py
+++ app_rd/do_reduction.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Author: Jiawei Ye
+Date: 2023-06-19 21:18:55
+LastEditors: Jiawei Ye
+LastEditTime: 2023-08-07 10:44:58
+FilePath: /rdcs/app_rd/do_reduction.py
+Description: 
+
+'''
 
 from model import Autoencoder
 import torch
@@ -6,7 +17,7 @@ class Reducer():
     def __init__(self, model_path, device) -> None:
         self.device = device
         self.model_path = model_path
-        self.model = Autoencoder(14, xcode_dim=3, with_bn=True)
+        self.model = Autoencoder(9, xcode_dim=3, with_bn=True)
         self.model.load_state_dict(torch.load(self.model_path, map_location=torch.device(self.device)))
         # self.model.load_state_dict(torch.load(self.model_path))
         self.model.to(self.device)
--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@
 Author: Jiawei Ye
 Date: 2023-06-19 21:18:55
 LastEditors: Jiawei Ye
-LastEditTime: 2023-08-03 16:31:16
+LastEditTime: 2023-08-07 11:41:36
 FilePath: /rdcs/test/test.py
 Description: 
 
@@ -12,17 +12,17 @@ Description:
 import requests
 import json
 # from swissknife import show_all_plots, plot_from_dict
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import base64
 import io
 import os
 from PIL import Image
-plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']  # 用来正常显示中文标签
-plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+# plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']  # 用来正常显示中文标签
+# plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 # 读取json文件
 print(os.getcwd())
-with open('test/request_gd2b_s.json', 'r') as f:
+with open('test/request_gd2b.json', 'r') as f:
     data = json.load(f)
 
 target_ip = os.getenv('TARGET_IP', '127.0.0.1')
@@ -37,36 +37,59 @@ anomaly = result.get('anomaly')
 cl_plots = result.get('cl_plots')
 kpi_plots = result.get('kpi_plots')
 
-def show_all_plots(all_plots):
-    for ne, plot_data in all_plots.items():
+print(kpi_plots.keys())
+
+# def show_all_plots(all_plots):
+#     for ne, plot_data in all_plots.items():
+#         # Decode the Base64 string to bytes
+#         image_bytes = base64.b64decode(plot_data)
+
+#         # Read the bytes as an image
+#         image = Image.open(io.BytesIO(image_bytes))
+
+#         # Display the image
+#         plt.figure(figsize=(10, 10))
+#         plt.imshow(image)
+#         plt.title(ne)
+#         plt.axis('off')  # Hide the axis
+#         plt.savefig(f'output/NE_{ne}.png')
+#         plt.close()
+
+# def plot_from_dict(result_dict):
+#     for ne, kpi_dict in result_dict.items():
+#         for kpi, image_base64 in kpi_dict.items():
+#             image_bytes = base64.b64decode(image_base64)
+#             image = Image.open(io.BytesIO(image_bytes))
+            
+#             plt.figure(figsize=(10, 5))
+#             plt.title(f'neName: {ne} - KPI: {kpi}')
+#             # plt.imshow(image)
+#             plt.axis('off')  # disable axis
+#             plt.savefig(f'output/NE_{ne}_KPI_{kpi}.png')
+#             plt.close()
+
+# show_all_plots(cl_plots)
+
+# plot_from_dict(kpi_plots)
+
+def save_xcode_fig(xcode_image_dict):
+    for ne, plot_data in xcode_image_dict.items():
         # Decode the Base64 string to bytes
         image_bytes = base64.b64decode(plot_data)
 
-        # Read the bytes as an image
-        image = Image.open(io.BytesIO(image_bytes))
+        filename = f'output/NE_{ne}.png'
+        with open(filename, 'wb') as f:
+            f.write(image_bytes)
 
-        # Display the image
-        plt.figure(figsize=(10, 10))
-        plt.imshow(image)
-        plt.title(ne)
-        plt.axis('off')  # Hide the axis
-        plt.savefig(f'output/NE_{ne}.png')
-        plt.close()
-
-def plot_from_dict(result_dict):
-    for ne, kpi_dict in result_dict.items():
+def save_kpi_fig(kpi_image_dict):
+    for ne, kpi_dict in kpi_image_dict.items():
         for kpi, image_base64 in kpi_dict.items():
             image_bytes = base64.b64decode(image_base64)
-            image = Image.open(io.BytesIO(image_bytes))
-            
-            plt.figure(figsize=(10, 5))
-            plt.title(f'neName: {ne} - KPI: {kpi}')
-            plt.imshow(image)
-            plt.axis('off')  # disable axis
-            plt.savefig(f'output/NE_{ne}_KPI_{kpi}.png')
-            plt.close()
 
-show_all_plots(cl_plots)
+            filename = f'output/NE_{ne}_KPI_{kpi}.png'
+            with open(filename, 'wb') as f:
+                f.write(image_bytes)
 
-plot_from_dict(kpi_plots)
+save_xcode_fig(cl_plots)
 
+save_kpi_fig(kpi_plots)
